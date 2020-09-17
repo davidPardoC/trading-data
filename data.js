0a96b184449a8ae93d93abc4125abe57010b6207
David Ernesto Pardo Cuenca <depardo3@gmail.com>
2020-09-17 09:25:04 -0500

    Decrement date

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
-for (let index = 0; index <=30; index++) {
-  
+let sec = 0;
+let data = [];
+for (let index = 0; index <=1800; index++) {
+  data.push({x: new Date(new Date().getFullYear(),new Date().getMonth(),new Date().getDay(),new Date().getHours(),new Date().getMinutes(),sec), open:Math.random() * (2 - 1) , high:Math.random() * (2 - 1), close:Math.random() * (2 - 1), low:Math.random() * (2 - 1), })
+  sec --;
 }
-console.log(new Date(Date.now()-1000))
-console.log(new Date(Date.now()-1000))
-console.log(new Date(Date.now()-1000))
+console.log(data);
