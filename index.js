765270505fa8f74197b05bb9f4333999e07a1e53
David Ernesto Pardo Cuenca <depardo3@gmail.com>
2020-09-17 10:05:17 -0500

    Fix date

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 app.get("/data", (req, res) => {
   let sec = 0;
   let data = [];
-  for (let index = 0; index <= 1800; index++) {
+  for (let index = 0; index <= 3600; index++) {
     data.push({
       x: new Date(
         new Date().getFullYear(),
@@ -25,14 +25,14 @@ app.get("/data", (req, res) => {
         new Date().getDay(),
         new Date().getHours(),
         new Date().getMinutes(),
-        sec
-      ).toISOString(),
+        new Date().getSeconds()-sec
+      ),
       open: Math.random() * (2 - 1)+1,
       high: Math.random() * (2 - 1)+1,
       close: Math.random() * (2 - 1)+1,
       low: Math.random() * (2 - 1)+1,
     });
-    sec--;
+    sec++;
   }
   res.json(data);
 });
