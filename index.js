6194c7752a94b6167beac4ab5f3398ff688adf21
David Ernesto Pardo Cuenca <depardo3@gmail.com>
2020-09-17 10:38:53 -0500

    Fix date

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 app.get("/data", (req, res) => {
   let sec = 0;
   let data = [];
-  for (let index = 0; index <= 1800; index++) {
+  for (let index = 0; index <= 300; index++) {
     data.push({
       x: new Date(
         new Date().getFullYear(),
