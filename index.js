591e4108f6df5f7d2f3ac910c6faedcf84e41730
David Ernesto Pardo Cuenca <depardo3@gmail.com>
2020-09-17 10:50:53 -0500

    Fix date

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ app.get("/data", (req, res) => {
         new Date().getMinutes(),
         new Date().getSeconds()-sec
       ),
-      open: Math.random() * (2 - 1)+1,
-      high: Math.random() * (2 - 1)+1,
-      close: Math.random() * (2 - 1)+1,
-      low: Math.random() * (2 - 1)+1,
+      open: Math.random() * (1.4 - 1)+1,
+      high: Math.random() * (1.4 - 1)+1,
+      close: Math.random() * (1.4 - 1)+1,
+      low: Math.random() * (1.4 - 1)+1,
     });
     sec++;
   }
